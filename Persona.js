28cc8ddda31d0bbde0a3cd0daea6e708da395e39
Franco Frizzo <francofrizzo@gmail.com>
2022-08-02 18:00:59 -0300

    Agrego ejercicios restantes

--- Persona.js
+++ Persona.js
@@ -1,13 +1,29 @@
 class Persona {
-  name = "";
+  name;
+  surname;
+  sex;
 
-  constructor(name) {
+  constructor(name, surname, sex) {
     this.name = name;
+    this.surname = surname;
+    this.sex = sex;
+  }
+
+  fullName() {
+    return this.name + " " + this.surname;
+  }
+
+  isMale() {
+    return this.sex === "male";
+  }
+
+  isFemale() {
+    return this.sex === "female";
+  }
+
+  toString() {
+    return this.fullName();
   }
 }
 
 const personas = [];
-
-personas.push(new Persona());
-
-console.log(personas);
--- script.js
+++ script.js
@@ -16,13 +16,35 @@ function handleAddBoxButtonClick() {
 
 function handleFormSubmit(event) {
   event.preventDefault();
-  alert("Enviando");
+  const name = event.target.querySelector("#name-field").value;
+  const surname = event.target.querySelector("#surname-field").value;
+  let sex;
+
+  if (event.target.querySelector("#sex-male-option").checked) {
+    sex = "male";
+  } else if (event.target.querySelector("#sex-female-option").checked) {
+    sex = "female";
+  }
+  const persona = new Persona(name, surname, sex);
+  personas.push(persona);
+
+  showPersonas();
 }
 
 function handleBoxClick(event) {
   if (event.target.className === "box") {
     alert("Box clicked");
   }
+}
+
+function showPersonas() {
+  document.getElementById("form-input").textContent = personas;
+
+  const men = personas.filter((persona) => persona.isMale());
+  const women = personas.filter((persona) => persona.isFemale());
+
+  document.getElementById("form-input-male-count").textContent = men.length;
+  document.getElementById("form-input-female-count").textContent = women.length;
 }
 
 document
@@ -33,3 +55,14 @@ document
 document
   .getElementsByClassName("boxes-container")[0]
   .addEventListener("click", handleBoxClick);
+
+function startTimer() {
+  const intervalId = setInterval(() => {
+    const timerCountElement = document.getElementById("timer-count");
+    const timerCount = parseInt(timerCountElement.textContent);
+    timerCountElement.textContent = timerCount - 1;
+    if (timerCount === 1) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
