b9ea622869d4cad7bfef20f9adaa29cf9124ffd5
gianzamboni <gianfranco376@gmail.com>
2022-10-04 14:05:55 -0300

    Basic template

--- Persona.js
+++ Persona.js
@@ -1,29 +1,5 @@
 class Persona {
-  name;
-  surname;
-  sex;
 
-  constructor(name, surname, sex) {
-    this.name = name;
-    this.surname = surname;
-    this.sex = sex;
-  }
-
-  fullName() {
-    return this.name + " " + this.surname;
-  }
-
-  isMale() {
-    return this.sex === "male";
-  }
-
-  isFemale() {
-    return this.sex === "female";
-  }
-
-  toString() {
-    return this.fullName();
-  }
 }
 
 const personas = [];
